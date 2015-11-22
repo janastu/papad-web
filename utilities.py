6dd7e4aa007d514a77e521501390d0e8a4fabed8
salus-sage <salus.sage@gmail.com>
2015-11-22 23:06:09 +0530

    Pymongo utilities

---
+++ utilities.py
@@ -0,0 +1,12 @@
+from pymongo.son_manipulator import SONManipulator
+
+
+class ObjectIdCleaner(SONManipulator):
+    def transform_outgoing(self, son, collection):
+        """Change the _id to be a string instance"""
+        if '_id' in son:
+            son['id'] = str(son['_id'])
+            del(son['_id'])
+        if 'passkey' in son:
+            del(son['passkey'])
+        return son
